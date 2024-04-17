971213af19875dba005f66f60941f8ce94f08d75
Song-yk <80368416+Song-yk@users.noreply.github.com>
2024-04-17 09:25:52 +0900

    [level 1] Title: 폰켓몬, Time: 4.16 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = nums.length/2;
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i = 0 ; i < nums.length; i++){
+            set.add(nums[i]);
+        }
+        
+        if(answer >= set.size()) answer = set.size();
+        
+        return answer;
+    }
+}
