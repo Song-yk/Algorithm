d65650c5d94876d5b36c7a2512c14b2659c2130a
Song-yk <80368416+Song-yk@users.noreply.github.com>
2024-08-20 17:52:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.80 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,40 +2,47 @@ import java.util.*;
 
 class Solution {
     static boolean [][] visited;
+    static int N,M;
     static int [] dx = {0, 1, 0, -1};
     static int [] dy = {1, 0, -1, 0};
-    static int N, M;
     static int answer = -1;
     public int solution(int[][] maps) {
         N = maps.length;
         M = maps[0].length;
-        visited = new boolean[N][M];
         
-        BFS(0, 0, maps);
+        visited = new boolean[N][M];
+        BFS(0,0, maps);
+        
+        
         return answer;
     }
-    public static void BFS(int i, int j, int [][] maps){
-        Queue<int[]> queue = new LinkedList<>();
+    
+    public void BFS(int i, int j, int [][] maps){
+        Queue<int []> queue = new LinkedList<>();
         queue.offer(new int[]{i,j});
         visited[i][j] = true;
-            while(!queue.isEmpty()){
-                int now [] = queue.poll();
-
-                if (now[0] == N-1 && now[1] == M-1 ) {
-                    answer = maps[N-1][M-1];
-                    return;
-                }
-                for(int k = 0; k <4; k++){
-                    int x = now[0] + dx[k];
-                    int y = now[1] + dy[k];
-                    if(x >= 0 && y >= 0 && x< N && y< M ){
-                        if(maps[x][y] !=0 && !visited[x][y]){
-                            visited[x][y] = true;
-                            maps[x][y] = maps[now[0]][now[1]] + 1;
-                            queue.add(new int[]{x,y});
-                        }
+        
+        while(!queue.isEmpty()){
+            int now [] = queue.poll();
+            
+            if(now[0] == N-1 && now[1] == M-1){
+                answer =  maps[N-1][M-1];
+                return;
+            }
+            
+            for(int k = 0; k <4; k++){
+                int x = now[0] + dx[k];
+                int y = now[1] + dy[k];
+                
+                if(x>=0 && y>=0 && x<N && y<M){
+                    if(!visited[x][y] && maps[x][y] == 1){
+                        visited[x][y] = true;
+                        maps[x][y] = maps[now[0]][now[1]] + 1;
+                        queue.add(new int [] {x,y});
                     }
                 }
             }
+        }
+        
     }
 }
