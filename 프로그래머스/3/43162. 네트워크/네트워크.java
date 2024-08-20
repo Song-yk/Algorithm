afe87a612c8125024c8c7c9b8b9ce075fd2b7ea6
Song-yk <80368416+Song-yk@users.noreply.github.com>
2024-08-20 15:04:57 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,29 @@
 class Solution {
-    public static int answer = 0;
-    public static boolean visited [];    
+    public static boolean [] visited;
+    public static int answer = 0; 
     public int solution(int n, int[][] computers) {
         visited = new boolean[computers.length];
         
-        for(int i = 0; i < n; i++){
+        for(int i = 0 ; i< computers.length;i++){
             if(!visited[i]){
                 answer++;
-                dfs(i, computers);
-                
+                DFS(i, computers);
             }
         }
         
         return answer;
     }
     
-    public void dfs(int Node, int [][] computers){
-        if(visited[Node]){
+    public void DFS(int node, int [][] computers){
+        if(visited[node]){
             return;
         }
-        
-        visited[Node] = true;
+        visited[node] = true;
         
         for(int i = 0 ; i < computers.length; i++){
-            if(!visited[i] && computers[Node][i] == 1){
-                dfs(i, computers);
+            if(!visited[i] && computers[node][i] == 1){
+                DFS(i, computers);
             }
         }
-        
-        return;
     }
 }
