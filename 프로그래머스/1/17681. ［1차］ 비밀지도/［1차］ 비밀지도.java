466f6d83e2073008c06e36502748a1c85fb9d54d
Song YuKyoung <80368416+Song-yk@users.noreply.github.com>
2024-08-27 09:42:05 +0900

    [level 1] Title: [1차] 비밀지도, Time: 3.35 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,7 +1,7 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String [n];
-        int [] count = new int [n];
+        
         for(int i = 0; i < n; i++){
            String a = Integer.toBinaryString(arr1[i] | arr2[i]);
            if(a.length() < n){
